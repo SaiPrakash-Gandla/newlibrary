95b764731150fd8ccad5ee022e85ae42706123e4
Saiprakash Gandla <87609708+SaiPrakash-Gandla@users.noreply.github.com>
2024-05-27 17:35:45 +0530

    deploy to tomcat

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -9,3 +9,7 @@ def artifact()
   sh "mvn package"
 }
 
+def deploytoTomcat(jobname, ip, context)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war   ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
