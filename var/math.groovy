2d0996238b36ffed719009b581b6f5a80538513b
Saiprakash Gandla <87609708+SaiPrakash-Gandla@users.noreply.github.com>
2024-05-27 13:59:58 +0530

    added basic functions arthamatic operator math.groovy

--- var/math.groovy
+++ var/math.groovy
@@ -1,1 +1,19 @@
+def sum(a, b)
+{
+  echo "the sume of ${a} and ${b} is %{a+b}"
+}
 
+def sub(a,b)
+{
+  echo "The subtraction of ${a} and ${b} is ${a-b}"
+}
+
+def mul(a,b)
+{
+  echo "The multipilication of ${a} and ${b} is ${a*b}"
+}
+
+def div(a,b)
+{
+  echo "The Division of ${a} and ${b} is ${a/b}"
+}
